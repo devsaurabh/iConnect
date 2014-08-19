9d00dcea0f8099484b58c5704d2e96e63aed6856
saurabh-singh-cardinalts <saurabh.singh@cardinalts.com>
2014-08-19 22:02:27 +0530

    Added reference to iConnect.Data in iConnect.Client

--- iConnect.Client/FriendList.xaml.cs
+++ iConnect.Client/FriendList.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using iConnect.Data;
+using iConnect.Data.ApplicationServices;
+using iConnect.Data.Model;
 
 namespace iConnect_Client
 {
@@ -22,6 +25,20 @@ namespace iConnect_Client
         public FriendList()
         {
             InitializeComponent();
+            FriendListBox.ItemsSource = GetUserList();
+        }
+
+        private IEnumerable<User> GetUserList()
+        {
+            var dataContext = new ChatContext();
+            
+            var users = new UserService(dataContext).GetAllUsers();
+            return users;
+        }
+
+        private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e)
+        {
+            
         }
     }
 }
